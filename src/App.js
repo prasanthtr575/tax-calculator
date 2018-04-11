819e45838f48c5b497e85f2c12c17a4c77b2b5fd
Alex Varghese <alex.varghese77@gmail.com>
2018-04-11 10:56:41 -0800

    redux added to the project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
+import allReducers from './dev/reducers/index'
+import {createStore,applyMiddleware,compose} from 'redux'
+import AppMain from './dev/components/AppMain'
+import {Provider} from 'react-redux'
 
 class App extends Component {
   render() {
+       const composeEnhancers =
+          typeof window === 'object' &&
+          window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+            window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+              // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+            }) : compose;
+
+        const enhancer = composeEnhancers(
+         //  applyMiddleware(...middleware),
+          // other store enhancers if any
+        );
+
+    const store = createStore(allReducers,enhancer)
     return (
-      <div className="App">
-        <header>
-          <h1>Tax Calculator</h1>
-        </header>
-        <p>
-          Work in Progress
-        </p>
-      </div>
+      <Provider store={store}>
+      <AppMain/>
+      </Provider>
     );
   }
 }
---
+++ src/dev/components/AppMain.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AppMain =()=>{
+return(
+    <div>
+        <header>
+          <h1>Tax Calculator</h1>
+        </header>
+        <p>
+          Work in Progress
+        </p>
+      </div>
+)
+}
+export default AppMain
