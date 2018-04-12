618164a388aa90148a3ffb4c4179620f314fb4eb
prasanthtr575 <mailprasanthtr@gmail.com>
2018-04-12 20:31:54 +0530

    Warning issue fix, removed unused variable

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
 import allReducers from './dev/reducers/index'
-import {createStore,applyMiddleware,compose} from 'redux'
+import { createStore, compose } from 'redux'
 import AppMain from './dev/components/AppMain'
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux'
 
 class App extends Component {
-  render() {
-       const composeEnhancers =
-          typeof window === 'object' &&
-          window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    render() {
+        const composeEnhancers =
+            typeof window === 'object' &&
+            window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
             window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-              // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+                // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
             }) : compose;
 
         const enhancer = composeEnhancers(
-         //  applyMiddleware(...middleware),
-          // other store enhancers if any
+            //  applyMiddleware(...middleware),
+            // other store enhancers if any
         );
 
-    const store = createStore(allReducers,enhancer)
-    return (
-      <Provider store={store}>
-      <AppMain/>
-      </Provider>
-    );
-  }
+        const store = createStore(allReducers, enhancer)
+        return (
+            <Provider store={store}>
+                <AppMain/>
+            </Provider>
+        );
+    }
 }
 
 export default App;
